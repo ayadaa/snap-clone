6c79b3d65b70fd272eaa6272bd3531d65b52ea06
okdevblog <okdevblog@gmail.com>
2025-07-28 06:01:41 +0000

    hghy

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -10,13 +10,23 @@ import { getFunctions } from 'firebase/functions';
  * Exports initialized services for use throughout the app.
  */
 
+// const firebaseConfig = {
+//   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+// };
+
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyADg_qwALwH0uczYEBfhOL-0nY_nWddjLQ",
+  authDomain: "snap-clone-2b5a1.firebaseapp.com",
+  projectId: "snap-clone-2b5a1",
+  storageBucket: "snap-clone-2b5a1.firebasestorage.app",
+  messagingSenderId: "623291393519",
+  appId: "1:623291393519:web:7a5ea1fb45acce1333821a",
+  // measurementId: "G-076PTSP8ZH"
 };
 
 // Initialize Firebase App
